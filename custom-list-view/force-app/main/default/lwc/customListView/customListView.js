016fe3fcddc22b275942b4f898ec2adcc0152c2f
Evan Klicker <evanklicker@gmail.com>
2024-10-11 12:16:31 -0500

    Got metadata-driven column defs and perm enforcement working

--- custom-list-view/force-app/main/default/lwc/customListView/customListView.js
+++ custom-list-view/force-app/main/default/lwc/customListView/customListView.js
@@ -1,8 +1,7 @@
 import { wire, LightningElement } from 'lwc';
 import generateData from './generateData';
 import { TableData } from './customListViewHelper';
-import getColumns from '@salesforce/apex/CustomListViewController.getColumns';
-import getRecords from '@salesforce/apex/CustomListViewController.getRecords';
+import getTableData from '@salesforce/apex/CustomListViewController.getTableData';
 import getUserData from '@salesforce/apex/CustomListViewController.getUserData';
 import saveUserData from '@salesforce/apex/CustomListViewController.saveUserData';
 
@@ -61,39 +60,86 @@ export default class DatatableWithInlineEdit extends LightningElement {
         return `${this.selectedRows.size} items selected`;
     }
     connectedCallback() {
-        this.data = generateData({ amountOfRecords: 305 });
-        if (!this.data || this.data.length === 0) { return; } // probably want to display an error in this case
-        this.datatableTitle = `Accounts (${this.data.length})`;
-        this.tableData = this.buildTableData({data: this.data, pageSize: this.pageSize});
-        this.pages = this.tableData.pages;
-        this.isLoading = false;
+        //this.data = generateData({ amountOfRecords: 305 });
+        //if (!this.data || this.data.length === 0) { return; } // probably want to display an error in this case
     }
 
-    @wire(getColumns) 
-    handleGetColumns({data, error}) {
+    @wire(getTableData) 
+    handleGetTableData({data, error}) {
+        console.log("handleGetTableData: ");
         if (error) { 
             this.error = error;
+            console.error(error);
         } else if (data) {
-            this.columns = data;
+            console.log(`getTableData result: `);
+            console.log(data);
+            if (!data.fields) { throw new Error('No column defs found!'); }
+            if (!data.records) { throw new Error('No records found!'); }
+            this.datatableTitle = `Accounts (${data.records.length})`;
+            this.columns = this.createColumnDefs(data.fields);
+            this.data = data.records;
+            this.tableData = this.buildTableData({data: this.data, pageSize: this.pageSize});
+            this.pages = this.tableData.pages;
             this.error = null;
-        }
-    }
-    @wire(getRecords) 
-    handleGetRecords({data, error}) {
-        if (error) { 
-            this.error = error;
-        } else if (data) {
-            this.data = data;
-            this.error = null;
+            this.isLoading = false;
         }
     }
     @wire(getUserData) 
     handleGetUserData({data, error}) {
+        console.log("handleGetUserData: "); 
         if (error) { 
             this.error = error;
+            console.error(error);
         } else if (data) {
+            console.log(data);
             this.userData = data;
             this.error = null;
+        }
+    }
+
+    // This doesn't handle relationship fields very well - they're all gonna be called "<object> Id" instead of "<object> Name" or just "<object>"
+    createColumnDefs(rawFieldInfos) {
+        return rawFieldInfos.map(field => {
+            // Remove the Name field since it's incorporated into the Id field def
+            // Probably will need to remove other relationship Name fields later, but they aren't in the field infos right now
+            if (field.name === 'Name') { return; }
+            let column = {
+                // label: field.name === 'Id'? 'Name' : field.label,
+                label: field.name === 'Id'? 'Name' : field.name,
+                type: this.convertType(field.type),
+                fieldName: field.name,
+                editable: field.isUpdateable, // this probably isn't the best way to do this since I don't think it considers user perms (though it should on the back-end when they try to save)
+                sortable: !!field.sortable
+            }
+            if (field.relationshipName) {
+                column.typeAttributes = {
+                    // When we get data back from the query, for relationship fields, it's gonna be like `Account.Name`
+                    // We're just gonna remove the `.` and use that at the label for the link to that record in the table
+                    // This also kinda assumes that the name field will be called name, which is generally but not always true
+                    label: { fieldName: `${field.relationshipName}Name`}
+                }
+                // This should work for all standard objects, but custom objects will need to have an icon listed in the column JSON
+                column.iconName = field.iconName || 'standard:' + field.sObjectType.toLowerCase();
+            }
+            console.dir(JSON.stringify(column));
+            return column;
+        }).filter(column => !!column);
+    }
+
+    convertType(type) {
+        switch (type) {
+            case 'boolean','currency':
+                return type;
+            case 'picklist','string','textarea','phone','address':
+                return 'text';
+            case 'int','double':
+                return 'number';
+            case 'reference', 'url':
+                return 'url';
+            case 'date','datetime':
+                return 'date';
+            default:
+                return 'text';
         }
     }
 
@@ -101,7 +147,7 @@ export default class DatatableWithInlineEdit extends LightningElement {
         return new TableData(data || this.data, pageSize || this.tableData.pageSize, filters, searchCriterion || this.searchTerm, sortOrder, sortField);
     }
 
-    // When the user types into the box, wait for 2 seconds before running the search to give them an opportunity to finish typing before removing their control
+    // When the user types into the box, wait for a bit before running the search to give them an opportunity to finish typing before removing their control
     handleSearchTermChanged(event) {
         if (this.searchTimeout) { window.clearTimeout(this.searchTimeout); }
         this.searchTimeout = window.setTimeout(() => {
